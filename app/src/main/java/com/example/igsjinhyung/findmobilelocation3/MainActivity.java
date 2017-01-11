db46c321afb8e568e25be177969600be6acb2589
iGSTaeHyung <thahn35@naver.com>
2017-01-11 20:31:48 +0900

    // 태형이 수정함.

--- app/src/main/java/com/example/igsjinhyung/findmobilelocation3/MainActivity.java
+++ app/src/main/java/com/example/igsjinhyung/findmobilelocation3/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // 현우가 수정함.
+        // 태형이 수정함.
     }
 }
